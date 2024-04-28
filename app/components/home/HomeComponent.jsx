bf23a4a536cfa492408d14296535f5d52955dcb7
Med Aziz Akaichi <135503030+AKAICHIaziz@users.noreply.github.com>
2024-04-28 10:08:21 +0100

    commited on 9:

--- app/components/home/HomeComponent.jsx
+++ app/components/home/HomeComponent.jsx
@@ -9,18 +9,20 @@ const HomeComponent = () => {
       <div className="profile">
 
         <div className="profile_info">
-          <div className="hi">Hi,</div>
-          <div className="name">I'm Akaichi Med Aziz</div>
-          <div className="description">A <span>Fullstack JS Developper</span></div>
+          <div className="text">
+            <div className="hi">Hi,</div>
+            <div className="name">I'm Akaichi Med Aziz</div>
+            <div className="description">A <span>Fullstack JS Developper</span></div> 
+          </div>
           <SocialMedia />
           <div className="buttons">
-            <button  className="btn bnt1"> Get in touch</button>
+            <button className="btn bnt1"> Get in touch</button>
             <button className="btn btn2">Hire me</button>
           </div>
         </div>
 
         <div className="profile_image">
-          <img  src="/me.jpg" alt="" />
+          <img src="/me.jpg" alt="" />
         </div>
 
       </div>
